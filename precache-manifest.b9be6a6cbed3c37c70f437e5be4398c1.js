86d16d91bf2826343c6d32a49914e481249d0f0a
codesque16 <shiladitya1601@gmail.com>
2021-09-20 11:57:48 +0530

    Updates

---
+++ precache-manifest.b9be6a6cbed3c37c70f437e5be4398c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1254edd90616ac9b4c528a2ffbb5cde",
+    "url": "/kyc_ocr/index.html"
+  },
+  {
+    "revision": "006d280e74485a5b0d85",
+    "url": "/kyc_ocr/static/css/2.b4c9628e.chunk.css"
+  },
+  {
+    "revision": "6e12b0de3d8a7412d4d9",
+    "url": "/kyc_ocr/static/css/main.7b7e429f.chunk.css"
+  },
+  {
+    "revision": "006d280e74485a5b0d85",
+    "url": "/kyc_ocr/static/js/2.e387b670.chunk.js"
+  },
+  {
+    "revision": "6e12b0de3d8a7412d4d9",
+    "url": "/kyc_ocr/static/js/main.aca209ba.chunk.js"
+  },
+  {
+    "revision": "9ba14e3ff6f5a2d00098",
+    "url": "/kyc_ocr/static/js/runtime~main.553dcd87.js"
+  }
+]);
